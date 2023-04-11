f496747b1fab3269ab8237a4b107f9037cab8725
giangtv83 <giangtv1712@gmail.com>
2023-04-11 21:20:30 +0700

    nodejs

---
+++ src/config/cloudinaryConfig.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+    cloud_name: "dmtxuvqdc",
+    api_key: "935375769852363",
+    api_secret: "0VZw7ha0ZJEJiK_6f_0r1Ehu5SQ",
+});
+
+export default cloudinary;
